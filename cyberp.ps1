681cc0b9a35ff78669e7fb47f5f724fa2c867642
SavageCabbage39 <100728985+SavageCabbage39@users.noreply.github.com>
2023-11-29 11:57:52 -0600

    Add files via upload

---
+++ cyberp.ps1
@@ -0,0 +1,13 @@
+#
+#
+#
+#       Property of Ty Chandrakasem
+#
+#
+# TODO: Make Chocoinstall actually work
+
+Write-Output "1. Password security and audit policies `n2. Services `n3. Firewall  and Defender config `n4. Chocoinstall(maybe) "
+
+$Task = Read-Host "Enter Task Number: "
+Write-Output "Selected Task $Task"
+
